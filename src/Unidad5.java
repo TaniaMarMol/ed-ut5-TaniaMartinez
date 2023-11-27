bd5ff7a0e28e570bed77aec83dc26ef0d46a5c7b
Pascual Galvan <josgalbar@alu.edu.gva.es>
2023-11-27 12:06:59 +0100

    Actualización
    Se actualizo el código de Tania, se añadió texto a la clase Unidad5.java

--- src/Unidad5.java
+++ src/Unidad5.java
@@ -2,7 +2,8 @@ public class Unidad5 {
     public static void main(String[] args) {
 
     System.out.println("Hola Soy Tania, este el mi proyecto unidad 5 Entornos de desarrollo");
-
+    System.out.println("Hola soy Pascual Galvañ y esta es mi aportacion al proyecto de Tania");
+    
     }
 
 }
